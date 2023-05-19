70e85b458d33ad8eaa2519f12e7c75233807522b
mohamed_elmnegged <120424034+omarabozied5@users.noreply.github.com>
2023-05-19 22:54:49 +0300

    Update ExpenseForm.js

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -3,20 +3,36 @@ import './ExpenseForm.css'
  
 const ExpensForm = ( ) =>
 { 
-    const [enteredTitle,setEnteredTitle] = useState('');
-    const [enteredAmount,setEnteredAmount] = useState('');
-    const [enteredDate,setEnteredDate] = useState('');
+    //const [enteredTitle,setEnteredTitle] = useState('');
+   // const [enteredAmount,setEnteredAmount] = useState('');
+    //const [enteredDate,setEnteredDate] = useState('');
+    const [userInput , setUserInput] = useState({
+        enterdTitle: '',
+        enterdAmount: '',
+        enterdDate: ''
+    })
     const titleChangeHandler = (event) =>
     {
-        setEnteredTitle(event.target.value);
+        //setEnteredTitle(event.target.value);
+        setUserInput({
+            ...userInput,
+            enterdTitle: event.target.value,
+        });
     }; 
     const amountChangeHandler = (event) =>
     {
-        setEnteredAmount(event.target.value);
-
+        // setEnteredAmount(event.target.value);
+        setUserInput({
+            ...userInput,
+            enterdAmount: event.target.value,
+        });
     }
     const dateChangeHandler = (event) =>{
-        setEnteredDate(event.target.value);
+       // setEnteredDate(event.target.value);
+       setUserInput({
+        ...userInput,
+        enterdDate: event.target.value,
+    });
     }
  
     return <form>
